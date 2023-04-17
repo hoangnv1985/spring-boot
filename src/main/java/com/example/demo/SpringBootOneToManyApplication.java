f3b41bcb901de0ead7a5f627b87de9a320e686bf
hoangnguyenv <hoangnv2101@gmail.com>
2023-04-17 14:26:29 +0700

    add log

--- src/main/java/com/example/demo/SpringBootOneToManyApplication.java
+++ src/main/java/com/example/demo/SpringBootOneToManyApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootOneToManyApplication {
+	private static final Logger logger = LogManager.getLogger(SpringBootOneToManyApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootOneToManyApplication.class, args);
+		logger.info("Finish start up");
 	}
 
 }
